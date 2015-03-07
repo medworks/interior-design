e8e2ad10553e56c9718f0f56e75a36bd61c0bdd5
mojiAm <amjadi.mojtaba@gmail.com>
2015-03-07 16:59:22 +0330

    seprate files

---
+++ inc/footer.php
@@ -0,0 +1,142 @@
+<div class="f-action" id="contactus">
+        <a href="#" class="f-cta f-cta-black" data-toggle="modal" data-target="#myModal">تماس با ما</a>
+    </div>
+            
+    <!-- Modal -->
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content text-center">
+                <div class="modal-header">
+                    <button type="button" class="close close-black" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">بستن</span></button>
+                </div>
+                <div class="modal-body">
+                    <!-- Contact Form (name, email, phone and message inputs for your email form "should change your email adress in contact.php file") -->     
+                    <div id="contact">                            
+                        <div id="message"></div>
+                        <form method="post" action="" name="contactform" id="contactform">
+                            <fieldset>
+                                <div class="col-md-12">
+                                    <!-- Name -->
+                                    <input kl_virtual_keyboard_secure_input="on" name="name" id="name" size="30" value="" placeholder="نام" type="text">
+                                    <br>
+                                    <!-- Email -->
+                                    <input kl_virtual_keyboard_secure_input="on" name="email" id="email" size="30" value="" placeholder="ایمیل" type="text">
+                                    <br> 
+                                    <!-- Phone -->
+                                    <input kl_virtual_keyboard_secure_input="on" name="phone" id="phone" size="30" value="" placeholder="تلفن" type="text">
+
+                                    <!-- Message -->                                                                        
+                                    <textarea name="comments" cols="40" rows="5" id="comments" placeholder="پیام"></textarea>
+                                     
+                                    <!-- Submit Button -->                                        
+                                    <button type="submit" class="btn black-btn blog-btn submit" id="submit" value="Submit">ارسال</button>
+                                </div>                                                                                                                                                             
+                            </fieldset>
+                        </form>                            
+                    </div>          
+                    <!-- End Contact Form -->
+                </div>                    
+            </div>
+        </div>
+    </div>     
+        
+    <!-- Footer -->  
+    <div class="footer-big">
+        <div class="container">
+            <div class="row">
+
+                <!-- About Us Information -->
+                <div class="col-md-3 col-sm-6">                           
+                    <div class="f-logo">
+                        <h2>دفاتر ما</h2>                                                                    
+                    </div>
+                </div>
+                
+                <!-- Recent Posts -->
+                <div class="col-md-3 col-sm-6 f-inner f-inner-black">                        
+                    <div class="col-sm-2">
+                        <i class="fa fa-map-marker fa-lg"></i> 
+                    </div>
+                    <div class="col-sm-10">
+                        <strong>مشهد</strong>
+                        <div class="f-space"></div>
+                        <address>                                
+                            سه راه فلسطین ساختمان 202<br>                                
+                            <abbr title="Phone"></abbr><span style="direction:ltr;display:inline-block"> (513) 766-6436<span><br />
+                            info@mediateq.ir
+                        </address>                           
+                    </div>
+                </div>
+
+                <!-- Latest Tweet -->
+                <div class="col-md-3 col-sm-6 f-inner f-inner-black">
+                    <div class="col-sm-2">
+                        <i class="fa fa-map-marker fa-lg"></i> 
+                    </div>
+                    <div class="col-sm-10">
+                        <strong>مشهد</strong>
+                        <div class="f-space"></div>
+                        <address>                                
+                            سه راه فلسطین ساختمان 202<br>                                
+                            <abbr title="Phone"></abbr><span style="direction:ltr;display:inline-block"> (513) 766-6436<span><br />
+                            info@mediateq.ir
+                        </address>    
+                    </div>
+                </div>
+
+                <!-- Recent Works -->
+                <div class="col-md-3 col-sm-6 f-inner f-inner-black">   
+                    <div class="col-sm-2">
+                        <i class="fa fa-map-marker fa-lg"></i> 
+                    </div>
+                    <div class="col-sm-10">
+                        <strong>مشهد</strong>
+                        <div class="f-space"></div>
+                        <address>                                
+                            سه راه فلسطین ساختمان 202<br>                                
+                            <abbr title="Phone"></abbr><span style="direction:ltr;display:inline-block"> (513) 766-6436<span><br />
+                            info@mediateq.ir
+                        </address>    
+                    </div>      
+                </div>                           
+
+            </div>
+        </div>
+    </div>   
+
+    <div id="footer">
+        <div class="container">   
+            <div class="row">                                                         
+                <!-- Copyright -->                                                       
+                <div class="col-sm-6 col-md-6 f-copyright">                        
+                    <span>© Copyright Mediateq.ir - All Rights Reserved</span>                        
+                </div>                      
+                <div class="col-sm-6 col-md-6">
+                    <!-- Social Icons -->                              
+                    <ul class="footer-social footer-social-black">
+                        <li><a href="#"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-facebook fa-stack-1x fa-inverse"></i></span></a></li>
+                        <li><a href="#"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-twitter fa-stack-1x fa-inverse"></i></span></a></li>
+                        <li><a href="#"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-skype fa-stack-1x fa-inverse"></i></span></a></li>
+                        <li><a href="#"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-pinterest fa-stack-1x fa-inverse"></i></span></a></li>
+                    </ul>
+                </div>
+            </div><!-- /row -->
+        </div><!-- /container -->
+    </div>        
+    <!-- End Footer -->               
+
+    <!-- jQuery Plugins -->                                 
+    <script src="./js/jquery-1.11.1.min.js"></script>                    
+    <script src="./js/bootstrap.min.js"></script>
+    <script src="./js/jquery.themepunch.tools.min.js"></script>   
+    <script src="./js/jquery.themepunch.revolution.min.js"></script> 
+    <script src="./js/jquery.cubeportfolio.min.js"></script>      
+    <script src="./js/owl.carousel.js"></script>
+    <script src="./js/moderniz.js"></script>    
+    <script src="./js/jquery.sticky.js"></script>   
+    <script src="./js/jquery.mmenu.min.js"></script>                     
+    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>     
+    <script src="./js/app.js"></script>                                
+    
+</body>
+</html>
