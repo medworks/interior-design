987a59525151da1af67f5eead8bc7ad1f2e50bc8
Saeid Hatami <hatami4510@gmail.com>
2015-03-13 15:42:26 +0330

    fix errors

--- manager/editwork.php
+++ manager/editwork.php
@@ -19,8 +19,8 @@
 	$db = Database::GetDatabase(); 
 	if ($_GET['act']=="del")
 	{
-		$db->Delete("articles"," id",$_GET["did"]);		
-		header('location:editarticle.php?act=new');	
+		$db->Delete("works"," id",$_GET["did"]);		
+		header('location:editwork.php?act=new');	
 	}		
     
 $html.=<<<cd
@@ -31,12 +31,12 @@ $html.=<<<cd
                 <div class="row">
                     <div class="col-md-12">
                         <!--Top header start-->
-                        <h3 class="ls-top-header"> ویرایش مقالات</h3>
+                        <h3 class="ls-top-header"> ویرایش نمونه کار ها</h3>
                         <!--Top header end -->
                         <!--Top breadcrumb start -->
                         <ol class="breadcrumb">
                             <li><a href="javascript:void(0);"><i class="fa fa-home"></i></a></li>
-                            <li class="active">ویرایش مقالات</li>
+                            <li class="active">ویرایش کار </li>
                         </ol>
                         <!--Top breadcrumb start -->
                     </div>
@@ -46,7 +46,7 @@ $html.=<<<cd
                         <div class="col-md-12">
                             <div class="panel panel-default">
                                 <div class="panel-heading">
-                                    <h3 class="panel-title">لیست مقالات</h3>
+                                    <h3 class="panel-title">لیست نمونه کار ها</h3>
                                 </div>
                                 <div class="panel-body">
                                     <!--Table Wrapper Start-->
@@ -72,16 +72,16 @@ cd;
 
 	$pagination->navigation_position("right");
 
-	$reccount = $db->CountAll("articles");
+	$reccount = $db->CountAll("works");
 	$pagination->records($reccount); 
 	
     $pagination->records_per_page($records_per_page);	
 
 $rows = $db->SelectAll(
-				"articles",
+				"works",
 				"*",
 				NULL,
-				"id ASC",
+				"regdate Desc",
 				($pagination->get_page() - 1) * $records_per_page,
 				$records_per_page);
 				
@@ -92,7 +92,7 @@ $rows[$i]["subject"] = strip_tags($rows[$i]["subject"]);
 $rows[$i]["body"] = strip_tags($rows[$i]["body"]);
 $rows[$i]["subject"] =(mb_strlen($rows[$i]["subject"])>40)?mb_substr($rows[$i]["subject"],0,40,"UTF-8")."...":$rows[$i]["subject"];
 $rows[$i]["body"] =(mb_strlen($rows[$i]["body"])>50)?mb_substr($rows[$i]["body"],0,50,"UTF-8")."...":$rows[$i]["body"];
-$pic = $db->Select("pics","*","idd='{$rows[$i][id]}' AND kind=1");
+$pic = $db->Select("pics","*","idd='{$rows[$i][id]}' AND kind=2");
 /*
 if (empty($pic["img"]))
 {
@@ -113,7 +113,7 @@ $html.=<<<cd
                                                 <td>{$rows[$i]["body"]}</td>
 												<td><img src='../{$pic[img]}' width='48px' height='48px' /> </td>
                                                 <td class="text-center">
-												<a href="addarticle.php?act=edit&did={$rows[$i]["id"]}"  >					
+												<a href="addwork.php?act=edit&did={$rows[$i]["id"]}"  >					
                                                     <button class="btn btn-xs btn-warning" title="ویرایش">
 														<i class="fa fa-pencil-square-o">
 														</i>
